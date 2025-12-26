54fb7c7bd98372a2120e9176b6cc16c5f2eeeb2b
Flow-Millot <florentin.millot@epfedu.fr>
2025-12-26 18:48:14 +0100

    First push
    Stable

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from .database import engine, Base
+from .routers import auth, beers, pages
+
+# Create tables on startup
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="PokeBeer")
+
+app.include_router(auth.router)
+app.include_router(beers.router)
+app.include_router(pages.router)
