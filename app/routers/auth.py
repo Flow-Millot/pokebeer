f6a313a04f011b28f5a429b90f33aea49a56bb55
Flow-Millot <florentin.millot@epfedu.fr>
2025-12-26 22:12:48 +0100

    cookie handle

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -17,11 +17,22 @@ def register(username: str = Form(...), password: str = Form(...), db: Session =
 def login(response: Response, username: str = Form(...), password: str = Form(...), db: Session = Depends(database.get_db)):
     user = crud.get_user_by_username(db, username)
     if not user or not auth.verify_password(password, user.hashed_password):
+        # On garde le RedirectResponse ici aussi
         return RedirectResponse(url="/login?error=Invalid Credentials", status_code=303)
     
     access_token = auth.create_access_token(data={"sub": user.username})
+    
+    # Création de la réponse de redirection
     response = RedirectResponse(url="/", status_code=303)
-    response.set_cookie(key="access_token", value=f"Bearer {access_token}", httponly=True)
+    
+    response.set_cookie(
+        key="access_token",
+        value=f"Bearer {access_token}",
+        httponly=True,
+        secure=True,
+        samesite="lax"
+    )
+    
     return response
 
 @router.get("/logout")
